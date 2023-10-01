e38a235a4550888c7bf125a37a5d8ef6f848e1c2
sanjave <ssingh025@citymail.cuny.edu>
2023-09-30 21:55:47 -0400

    Day 26: Add a table

--- PDF_Invoices.py
+++ PDF_Invoices.py
@@ -7,8 +7,6 @@ filepaths = glob.glob("invoices/*xlsx")
 
 # Iterate through a list of file paths
 for filepath in filepaths:
-    # Read data from an Excel file into a DataFrame
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
     # Create a PDF object with specified orientation, unit, and format
     pdf = FPDF(orientation="P", unit="mm", format="A4")
@@ -29,8 +27,31 @@ for filepath in filepaths:
     # Set font properties for the PDF content
     pdf.set_font(family="Times", size=14, style="B")
     # Add a cell to the PDF with the formatted date
-    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+
+    # Read data from an Excel file into a DataFrame
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Add a header
+    columns = list(df.columns)
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1, align='C')  # Center aligned text
+    pdf.cell(w=70, h=8, txt=columns[1], border=1, align='C')  # Center aligned text
+    pdf.cell(w=30, h=8, txt=columns[2], border=1, align='C')  # Center aligned text
+    pdf.cell(w=30, h=8, txt=columns[3], border=1, align='C')  # Center aligned text
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1, align='C')  # Center aligned text
+
+    # Add rows to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1, align='C')  # Center aligned text
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1, align='C')  # Center aligned text
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1, align='C')  # Center aligned text
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1, align='C')  # Center aligned text
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1, align='C')  # Center aligned text
 
     # Output the PDF file to the specified directory with the formatted filename
     pdf.output(f"PDFs/{filename}.pdf")
-

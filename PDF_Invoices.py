75a72eefe8b71e40de57320611498b3c39ec8051
sanjave <ssingh025@citymail.cuny.edu>
2023-09-30 22:08:59 -0400

    Day 26: Add header, total price, company and logo

--- PDF_Invoices.py
+++ PDF_Invoices.py
@@ -53,5 +53,23 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1, align='C')  # Center aligned text
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1, align='C')  # Center aligned text
 
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1, align='C')  # Center aligned text
+    pdf.cell(w=70, h=8, txt="", border=1, align='C')  # Center aligned text
+    pdf.cell(w=30, h=8, txt="", border=1, align='C')  # Center aligned text
+    pdf.cell(w=30, h=8, txt="", border=1, align='C')  # Center aligned text
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1, align='C')  # Center aligned text
+
+    # Add total sum sentence
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is: {total_sum}",ln=1)
+
+    # Add Company Name and Logo
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=25, h=8, txt=f"PythonHow")
+    pdf.image("pythonhow.png", w=10)
+
     # Output the PDF file to the specified directory with the formatted filename
     pdf.output(f"PDFs/{filename}.pdf")
